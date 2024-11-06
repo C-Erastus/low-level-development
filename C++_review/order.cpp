bc2e9c5cce7ddd89ff3b651c417c6ed57be6b2bf
C-Erastus <chukpozohnt@gmail.com>
2024-11-05 23:19:26 -0500

    working on the great circle

--- C++_review/order.cpp
+++ C++_review/order.cpp
@@ -4,10 +4,14 @@ int main(int argc, char **argv){
 
 	bool val;
 
-	if((argv[1] < argv[2]) && (argv[2] < argv[3]))
+	if(( atoi(argv[1]) < atoi(argv[2])) && (atoi(argv[2]) < atoi(argv[3]))){
 		std::cout << "true" << std::endl; 
-	else if ((argv[1] > argv[2]) && (argv[2] > argv[3]))
+		std::cout << "part 1" << std::endl;
+	}
+	else if ((atoi(argv[1]) > atoi(argv[2])) && (atoi(argv[2]) > atoi(argv[3]))){
 		std::cout << "true" << std::endl;
+		std::cout << "part 2" << std::endl;
+	}
 	else
 		std::cout << "false" << std::endl;
 
