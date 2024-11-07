88968bfc6bd36be4896c9f9428b81dc0a31dc7dc
C-Erastus <chukpozohnt@gmail.com>
2024-11-06 23:05:20 -0500

    Great circle

--- C++_review/GreatCircle.cpp
+++ C++_review/GreatCircle.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
+#include <cmath>
 int main(int argc, char **argv)
 {
-	float 
+	float x1, y1, x2, y2, distance;
+
+	x1 = atof(argv[1]); 
+	y1 = atof(argv[2]);
+	x2 = atof(argv[3]);
+	y2 = atof(argv[4]);  
+
+	distance = ((sin(x1) * sin(x2)) + (cos(x1) * cos(x2) * (cos(y1 - y2))));
+
+	std::cout << distance << std::endl;
+
 	return 0; 
 }
