48d4951fb42aced1bcb2b40206690d277b6c8e20
C-Erastus <chukpozohnt@gmail.com>
2024-11-09 22:47:32 -0500

    finished bits but not realy bits

--- C++_review/Bits.cpp
+++ C++_review/Bits.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
 	int n = atoi(argv[1]);
 	int bits = 0;
 
-	if (n < 0)
+	if (n < 1) {
 		std::cout << "Illegal input" << std::endl; 
-	else if (n > 0 ){
-		bits += 1;
+		exit(1);
 	}
-	else
-		bits = 0;
-
-	while(n > 1){
-		bits += (n % 2); 
+	while (n > 0) {
+		bits++;
 		n = n/2;
 	}
 
